5a48ffb9cb2ad99950f9ca5ce45e902e31ca7e36
enessenws <enes_bu26@hotmail.com>
2023-08-22 15:55:25 +0300

    first commit

---
+++ src/main/java/com/eneskyc/vcard/Vcard.java
@@ -0,0 +1,15 @@
+package com.eneskyc.vcard;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Vcard  {
+
+    @GetMapping
+    String getVcard(Model model) {
+        model.addAttribute("something", "Vcard için ilk yazı");
+        return "vcard" ;
+    }
+}
---
+++ src/test/java/com/eneskyc/vcard/VcardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eneskyc.vcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
